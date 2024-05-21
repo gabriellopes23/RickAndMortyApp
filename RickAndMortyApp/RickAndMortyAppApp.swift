0b63fcecc2f23a9cc88310b651f6b2baca542ad7
Gabriel Lopes <lopessantosgabriel@outlook.com>
2024-05-21 08:36:50 -0300

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Gabriel Lopes on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
