dbec5fd2dd7265f81b6daca6c914842a3053a015
Gabriel Lopes <lopessantosgabriel@outlook.com>
2024-05-23 12:02:51 -0300

    Terminei de integrar o LocationListView, conclui o EpisodeListView

---
+++ RickAndMortyApp/View/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  RickAndMortyApp
+//
+//  Created by Gabriel Lopes on 23/05/24.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
